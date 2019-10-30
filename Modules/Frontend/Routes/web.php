6fe5a5ef727d4e44b7d8acb1b8212ca3d0d127f1
nguyenthanhtung74119@gmail.com <1406199774119>
2019-10-30 09:28:36 +0700

    gv

--- Modules/Frontend/Routes/web.php
+++ Modules/Frontend/Routes/web.php
@@ -20,10 +20,6 @@ Route::group(['middleware' => ['locale']], function () {
     Route::group(['prefix' => 'vi', 'lang' => 'vi'], function () {
         Route::get('/', 'IndexController@index')->name('frontend.home_vi');
         Route::post('/', 'IndexController@submitContact')->name('frontend.home_vi');
-        //submit contact vi
-        Route::get('/san-pham/{product}', 'ProductController@index')->name('frontend.product.detail_vi');
-        Route::get('/giai-phap/{solution}', 'SolutionController@index')->name('frontend.solution.detail_vi');
-
         // Giới thiệu
         Route::get('gioi-thieu', 'AboutUsController@index')->name('frontend.about_vi');
         // Company
@@ -32,63 +28,77 @@ Route::group(['middleware' => ['locale']], function () {
         // Business outline
         Route::get('doanh-nghịep', 'AboutUsController@business')->name('frontend.business_vi');
         // !Business outline
-        // Công nghệ của chúng tôi
-        Route::get('cong-nghe-cua-chung-toi', 'AboutUsController@ourTechnology')
-            ->name('frontend.about.our_technology_vi');
-
-        // Tin tức
-        Route::get('tin-tuc', 'AboutUsController@news')->name('frontend.about.news_vi');
-        Route::get('tin-tuc/{alias}', 'AboutUsController@newsDetail')->name('frontend.about.news_detail_vi');
-        Route::get('tin-tuc/danh-muc/{alias}', 'AboutUsController@newsCategory')
-            ->name('frontend.about.news_category_vi');
-
+        // solution-cat
+        Route::get('nhom-giai-phap', 'AboutUsController@solution-cat')->name('frontend.solution-cat_vi');
+        // !solution-cat
+        // solution
+        Route::get('giai-phap', 'AboutUsController@solution')->name('frontend.solution_vi');
+        // !solution
+        // maintenance
+        Route::get('bao-tri', 'AboutUsController@maintenance')->name('frontend.maintenance_vi');
+        // !maintenance
         //Liên hệ
         Route::get('lien-he-chung-toi', 'AboutUsController@contact')->name('frontend.about.contact_vi');
         Route::post('lien-he-chung-toi', 'AboutUsController@submitContact')->name('frontend.about.contact_vi');
 
+        //submit contact vi
+//        Route::get('/san-pham/{product}', 'ProductController@index')->name('frontend.product.detail_vi');
+//        Route::get('/giai-phap/{solution}', 'SolutionController@index')->name('frontend.solution.detail_vi');
+        // Công nghệ của chúng tôi
+//        Route::get('cong-nghe-cua-chung-toi', 'AboutUsController@ourTechnology')
+//            ->name('frontend.about.our_technology_vi');
+        // Tin tức
+//        Route::get('tin-tuc', 'AboutUsController@news')->name('frontend.about.news_vi');
+//        Route::get('tin-tuc/{alias}', 'AboutUsController@newsDetail')->name('frontend.about.news_detail_vi');
+//        Route::get('tin-tuc/danh-muc/{alias}', 'AboutUsController@newsCategory')
+//            ->name('frontend.about.news_category_vi');
         // Partner
-        Route::get('doi-tac', 'PartnerController@index')->name('frontend.partner_vi');
-        Route::get('dai-ly-tpcloud', 'PartnerController@agent')->name('frontend.partner.agent_vi');
-
+//        Route::get('doi-tac', 'PartnerController@index')->name('frontend.partner_vi');
+//        Route::get('dai-ly-tpcloud', 'PartnerController@agent')->name('frontend.partner.agent_vi');
         //Support
-        Route::get('trung-tam-ho-tro', 'SupportController@index')->name('frontend.support_vi');
-        Route::get('cau-hoi-thuong-gap', 'SupportController@faq')->name('frontend.support.faq_vi');
+//        Route::get('trung-tam-ho-tro', 'SupportController@index')->name('frontend.support_vi');
+//        Route::get('cau-hoi-thuong-gap', 'SupportController@faq')->name('frontend.support.faq_vi');
     });
 
     Route::group(['prefix' => 'en', 'lang' => 'en'], function () {
         Route::get('/', 'IndexController@index')->name('frontend.home_en');
         Route::post('/', 'IndexController@submitContact')->name('frontend.home_en');
-        Route::get('/product/{product}', 'ProductController@index')->name('frontend.product.detail_en');
-        Route::get('/solution/{solution}', 'SolutionController@index')->name('frontend.solution.detail_en');
         // Giới thiệu
         Route::get('about-us', 'AboutUsController@index')->name('frontend.about_en');
         // Company
         Route::get('company', 'AboutUsController@company')->name('frontend.company_en');
         // !Company
-
         // Business outline
         Route::get('business', 'AboutUsController@business')->name('frontend.business_en');
         // !Business outline
-        // Công nghệ của chúng tôi
-        Route::get('our-technology', 'AboutUsController@ourTechnology')
-            ->name('frontend.about.our_technology_en');
-
-        // Tin tức
-        Route::get('news', 'AboutUsController@news')->name('frontend.about.news_en');
-        Route::get('news/{alias}', 'AboutUsController@newsDetail')->name('frontend.about.news_detail_en');
-        Route::get('news/category/{alias}', 'AboutUsController@newsCategory')->name('frontend.about.news_category_en');
-
+        // solution-cat
+        Route::get('solution-cat', 'AboutUsController@solution-cat')->name('frontend.solution-cat_en');
+        // !solution-cat
+        // solution
+        Route::get('solution', 'AboutUsController@solution')->name('frontend.solution_en');
+        // !solution
+        // maintenance
+        Route::get('maintenance', 'AboutUsController@maintenance')->name('frontend.maintenance_en');
+        // !maintenance
         //Liên hệ
         Route::get('contact-us', 'AboutUsController@contact')->name('frontend.about.contact_en');
         Route::post('contact-us', 'AboutUsController@submitContact')->name('frontend.about.contact_en');
 
+        // Công nghệ của chúng tôi
+//        Route::get('our-technology', 'AboutUsController@ourTechnology')
+//            ->name('frontend.about.our_technology_en');
+        // Tin tức
+//        Route::get('news', 'AboutUsController@news')->name('frontend.about.news_en');
+//        Route::get('news/{alias}', 'AboutUsController@newsDetail')->name('frontend.about.news_detail_en');
+//        Route::get('news/category/{alias}', 'AboutUsController@newsCategory')->name('frontend.about.news_category_en');
+//        Route::get('/product/{product}', 'ProductController@index')->name('frontend.product.detail_en');
+//        Route::get('/solution/{solution}', 'SolutionController@index')->name('frontend.solution.detail_en');
         // Partner
-        Route::get('partner', 'PartnerController@index')->name('frontend.partner_en');
-        Route::get('agent-tpcloud', 'PartnerController@agent')->name('frontend.partner.agent_en');
-
+//        Route::get('partner', 'PartnerController@index')->name('frontend.partner_en');
+//        Route::get('agent-tpcloud', 'PartnerController@agent')->name('frontend.partner.agent_en');
         // Support
-        Route::get('support-central', 'SupportController@index')->name('frontend.support_en');
-        Route::get('faq', 'SupportController@faq')->name('frontend.support.faq_en');
+//        Route::get('support-central', 'SupportController@index')->name('frontend.support_en');
+//        Route::get('faq', 'SupportController@faq')->name('frontend.support.faq_en');
     });
 });
 
