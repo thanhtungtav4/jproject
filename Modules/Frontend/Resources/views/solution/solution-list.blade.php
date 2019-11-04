4a065ebaa573761137a554bc6448a17bce44d439
Phu <lequangphu24031994@gmail.com>
2019-11-04 15:06:34 +0700

    fix text and css

--- Modules/Frontend/Resources/views/solution/solution-list.blade.php
+++ Modules/Frontend/Resources/views/solution/solution-list.blade.php
@@ -1,40 +1,40 @@
 @extends('frontend::layouts.master')
 {{--@section('title', $page[getValueByLang('page_title_')])--}}
-@section('title', $page[0][getValueByLang('page_sub_title_1_')])
+@section('title', __("Giải pháp"))
 @section('content')
-    @include('frontend::inc.banner', ['data' => $page[0][getValueByLang('page_sub_title_1_')]])
+    @include('frontend::inc.banner', ['data' => __("Giải pháp")])
     <section class="main_content">
         <div class="container">
             {{--$page[0][getValueByLang('page_sub_title_1_')])--}}
             <div class="row">
-                <div class="col-md-3">
-                    <strong class="profile_title">{{$page[0][getValueByLang('page_sub_title_1_')]}}</strong>
+                <div class="col-md-4">
+                    <strong class="profile_title">{{__("Giải pháp")}}</strong>
                     <ul class="nav">
                         <?php $tmp = 0 ?>
                         <?php $category = 0 ?>
                         @foreach($page as $value)
                             @if($tmp == 0)
                                 <li class="nav-item">
-                                    <a class="nav-link active" data-toggle="tab" href="#tab_{{$value['page_id']}}">{{$value[getValueByLang('page_sub_title_1_')]}}</a>
+                                    <a class="nav-link active bold-text" data-toggle="tab" href="#tab_{{$value['page_id']}}">{{$value[getValueByLang('page_sub_title_1_')]}}</a>
                                     <?php $tmp = 1 ?>
                                 </li>
                             @else
                                 @if($value['page_type'] == 'solution-list-child')
                                     @if($category == 0)
                                         <li class="nav-item">
-                                            <a class="nav-link " data-toggle="tab" href="javascript:void(0)">{{__("Tiết kiệm năng lượng hệ thống DHKK của RiCS")}}</a>
+                                            <a class="nav-link bold-text" data-toggle="tab" href="javascript:void(0)">{{__("Tiết kiệm năng lượng hệ thống DHKK của RiCS")}}</a>
                                             <?php $category = 1 ?>
                                         </li>
                                     @endif
                                 @endif
                                 <li class="nav-item">
-                                    <a class="nav-link {{$value['page_type'] == 'solution-list-child' ? 'child-nav' : ''}}" data-toggle="tab" href="#tab_{{$value['page_id']}}">{{$value[getValueByLang('page_sub_title_1_')]}}</a>
+                                    <a class="nav-link {{$value['page_type'] == 'solution-list-child' ? 'child-nav' : 'bold-text'}}" data-toggle="tab" href="#tab_{{$value['page_id']}}">{{$value[getValueByLang('page_sub_title_1_')]}}</a>
                                 </li>
                             @endif
                         @endforeach
                     </ul>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-8">
                     <div class="tab-content">
                         <?php $tmp1 = 0 ?>
                         @foreach($page as $value)
